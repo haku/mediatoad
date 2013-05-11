c21b23545e1a96ea09b39c18e1be458f02e53a08
はく <haku@vaguehope.com>
2013-05-11 14:43:11 +0100

    Show services of discovered devices.

--- src/main/java/com/vaguehope/dlnatoad/dlnaserver/DeviceWatcher.java
+++ src/main/java/com/vaguehope/dlnatoad/dlnaserver/DeviceWatcher.java
@@ -3,6 +3,7 @@ package com.vaguehope.dlnatoad.dlnaserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.teleal.cling.model.meta.RemoteDevice;
+import org.teleal.cling.model.meta.RemoteService;
 import org.teleal.cling.registry.DefaultRegistryListener;
 import org.teleal.cling.registry.Registry;
 
@@ -13,17 +14,31 @@ public class DeviceWatcher extends DefaultRegistryListener {
 	public DeviceWatcher () {}
 
 	@Override
-	public void remoteDeviceAdded (Registry registry, RemoteDevice device) {
-		logEvent(device, "found");
+	public void remoteDeviceAdded (final Registry registry, final RemoteDevice device) {
+		LOG.info("found: {} {} {}",
+				device.getIdentity().getDescriptorURL().getHost(),
+				device.getDisplayString(),
+				serviceTypes(device).toString()
+				);
 	}
 
 	@Override
-	public void remoteDeviceRemoved (Registry registry, RemoteDevice device) {
-		logEvent(device, "lost");
+	public void remoteDeviceRemoved (final Registry registry, final RemoteDevice device) {
+		LOG.info("lost: {} {}",
+				device.getIdentity().getDescriptorURL().getHost(),
+				device.getDisplayString()
+				);
 	}
 
-	private static void logEvent (RemoteDevice device, String verb) {
-		LOG.info(verb + ": {} {}", device.getClass().getSimpleName(), device.getIdentity().getDescriptorURL().getHost());
+	private static StringBuilder serviceTypes (final RemoteDevice device) {
+		final StringBuilder svcsDes = new StringBuilder("[");
+		final RemoteService[] svcs = device.getServices();
+		for (final RemoteService svc : svcs) {
+			if (svcsDes.length() > 1) svcsDes.append(", ");
+			svcsDes.append(svc.getServiceType().getType());
+		}
+		svcsDes.append("]");
+		return svcsDes;
 	}
 
 }
