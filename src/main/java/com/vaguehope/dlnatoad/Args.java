8137673d5478f8fddace1d1e3a11c512255ddc02
はく <haku@vaguehope.com>
2014-03-22 18:21:13 +0000

    Add CLI arg --tree for specifying a list of dirs to scan in a file.

--- src/main/java/com/vaguehope/dlnatoad/Args.java
+++ src/main/java/com/vaguehope/dlnatoad/Args.java
@@ -1,30 +1,46 @@
 package com.vaguehope.dlnatoad;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.Option;
 
 public class Args {
 
-	@Option(name = "-r", aliases = { "--refresh" }) private boolean refresh;
-
+	@Option(name = "-r", aliases = { "--refresh" }, usage = "refresh every " + C.REFRESH_INTERVAL_MINUTES + " minutes.") private boolean refresh;
+	@Option(name = "-t", aliases = { "--tree" }, metaVar = "<file>", usage = "file root dirs to scan, one per line.") private String treePath;
 	@Argument(multiValued = true, metaVar = "DIR") private List<String> dirPaths;
 
-	public List<File> getDirs () throws CmdLineException {
-		List<File> files;
-		if (this.dirPaths == null || this.dirPaths.isEmpty()) {
-			files = new ArrayList<File>();
-			files.add(new File("."));
+	public List<File> getDirs () throws CmdLineException, IOException {
+		final List<File> dirs = new ArrayList<File>();
+
+		if (this.treePath != null && this.treePath.length() > 0) {
+			final File treeFile = new File(this.treePath);
+			if (!treeFile.exists()) throw new CmdLineException(null, "File not found: " + this.treePath);
+			if (!treeFile.isFile()) throw new CmdLineException(null, "Not a file: " + this.treePath);
+			for (final String line : FileUtils.readLines(treeFile)) {
+				if (line.length() < 1 || line.startsWith("#")) continue;
+				final File lineDir = new File(line);
+				if (!lineDir.exists()) throw new CmdLineException(null, "Directory not found: " + line);
+				if (!lineDir.isDirectory()) throw new CmdLineException(null, "Not a directory: " + line);
+				dirs.add(lineDir);
+			}
 		}
-		else {
-			files = pathsToFiles(this.dirPaths);
-			checkDirExist(files);
+
+		if (this.dirPaths != null && this.dirPaths.size() > 0) {
+			final List<File> cliDirs = pathsToFiles(this.dirPaths);
+			checkDirExist(cliDirs);
+			dirs.addAll(dirs);
 		}
-		return files;
+
+		if (dirs.size() < 1) dirs.add(new File("."));
+
+		return dirs;
 	}
 
 	public boolean isRefresh () {
