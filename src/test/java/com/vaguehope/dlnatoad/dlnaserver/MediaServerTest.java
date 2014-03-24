efb9cf898de69536f93ce0264588ad79d16a9b77
はく <haku@vaguehope.com>
2014-03-24 11:25:43 +0000

    Placeholder test for MediaServer.

---
+++ src/test/java/com/vaguehope/dlnatoad/dlnaserver/MediaServerTest.java
@@ -0,0 +1,12 @@
+package com.vaguehope.dlnatoad.dlnaserver;
+
+import org.junit.Test;
+
+public class MediaServerTest {
+
+	@Test
+	public void itDoesSomething () throws Exception {
+		new MediaServer(new ContentTree(), "hostname");
+	}
+
+}
