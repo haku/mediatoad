3778d643553646847e5925849d6fc68325258a58
haku <git@haku.me>
2014-10-12 17:56:53 +0100

    Very hacky initial search impl. [issue #4]

--- src/main/java/com/vaguehope/dlnatoad/dlnaserver/ContentDirectoryService.java
+++ src/main/java/com/vaguehope/dlnatoad/dlnaserver/ContentDirectoryService.java
@@ -1,9 +1,16 @@
 package com.vaguehope.dlnatoad.dlnaserver;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.teleal.cling.model.types.ErrorCode;
 import org.teleal.cling.support.contentdirectory.AbstractContentDirectoryService;
 import org.teleal.cling.support.contentdirectory.ContentDirectoryErrorCode;
 import org.teleal.cling.support.contentdirectory.ContentDirectoryException;
@@ -13,6 +20,7 @@ import org.teleal.cling.support.model.BrowseResult;
 import org.teleal.cling.support.model.DIDLContent;
 import org.teleal.cling.support.model.SortCriterion;
 import org.teleal.cling.support.model.container.Container;
+import org.teleal.cling.support.model.item.Item;
 
 /**
  * Based on a class from WireMe and used under Apache 2 License. See
@@ -20,25 +28,39 @@ import org.teleal.cling.support.model.container.Container;
  */
 public class ContentDirectoryService extends AbstractContentDirectoryService {
 
+	// error codes from UPnP-av-ContentDirectory-v1-Service
+	private static final int NO_SUCH_OBJECT = 701; // The specified ObjectID is invalid.
+	private static final int UNSUPPORTED_SEARCH_CRITERIA = 708; // The search criteria specified is not supported or is invalid.
+	private static final int UNSUPPORTED_SORT_CRITERIA = 709; // The sort criteria specified is not supported or is invalid.
+	private static final int UNSUPPORTED_SEARCH_CONTAINER = 710; // The specified ContainerID is invalid or identifies an object that is not a container.
+	private static final int RESTRICTED_OBJECT = 711; // Operation failed because the restricted attribute of object is set to true.
+	private static final int BAD_METADATA = 712; // Operation fails because it would result in invalid or disallowed metadata in current object.
+	private static final int RESTRICTED_PARENT_OBJECT = 713; // Operation failed because the restricted attribute of parent object is set to true.
+	private static final int CANNOT_PROCESS = 720; // Cannot process the request.
+
 	private static final Logger LOG = LoggerFactory.getLogger(ContentDirectoryService.class);
 
 	private final ContentTree contentTree;
 
 	public ContentDirectoryService (final ContentTree contentTree) {
-		super();
+		super(
+				Arrays.asList("dc:title", "upnp:class"), // also "dc:creator", "dc:date", "res@size"
+				Arrays.asList("dc:title")); // also "dc:creator", "dc:date", "res@size"
 		this.contentTree = contentTree;
 	}
 
+	/**
+	 * Root is requested with objectID="0".
+	 */
 	@Override
 	public BrowseResult browse (final String objectID, final BrowseFlag browseFlag, final String filter, final long firstResult, final long maxResults, final SortCriterion[] orderby) throws ContentDirectoryException {
 		LOG.info("browse: {} ({}, {})", objectID, firstResult, maxResults);
 		try {
-			final DIDLContent didl = new DIDLContent();
 			final ContentNode contentNode = this.contentTree.getNode(objectID);
-
 			if (contentNode == null) return new BrowseResult("", 0, 0);
 
 			if (contentNode.isItem()) {
+				final DIDLContent didl = new DIDLContent();
 				didl.addItem(contentNode.getItem());
 				return new BrowseResult(new DIDLParser().generate(didl), 1, 1);
 			}
@@ -46,38 +68,92 @@ public class ContentDirectoryService extends AbstractContentDirectoryService {
 			final Container contentContainer = contentNode.getContainer();
 
 			if (browseFlag == BrowseFlag.METADATA) {
+				final DIDLContent didl = new DIDLContent();
 				didl.addContainer(contentContainer);
 				return new BrowseResult(new DIDLParser().generate(didl), 1, 1);
 			}
 
-			if (contentContainer.getContainers().size() > firstResult) {
-				final int from = (int) firstResult;
-				final int to = Math.min((int) (firstResult + maxResults), contentContainer.getContainers().size());
-				didl.setContainers(contentContainer.getContainers().subList(from, to));
-			}
-			if (didl.getContainers().size() < maxResults) {
-				final int from = (int) Math.max(firstResult - contentContainer.getContainers().size(), 0);
-				final int to = Math.min(contentContainer.getItems().size(), from + (int) (maxResults - didl.getContainers().size()));
-				didl.setItems(contentContainer.getItems().subList(from, to));
-			}
-			return new BrowseResult(new DIDLParser().generate(didl),
-					didl.getContainers().size() + didl.getItems().size(),
-					contentContainer.getChildCount().intValue());
+			return toRangedResult(contentContainer.getContainers(), contentContainer.getItems(), firstResult, maxResults);
 		}
 		catch (final Exception e) {
 			LOG.warn(String.format("Failed to generate directory listing" +
 					" (objectID=%s, browseFlag=%s, filter=%s, firstResult=%s, maxResults=%s, orderby=%s).",
-					objectID, browseFlag, filter, firstResult, maxResults, Arrays.toString(orderby)),
-					e);
+					objectID, browseFlag, filter, firstResult, maxResults, Arrays.toString(orderby)), e);
 			throw new ContentDirectoryException(ContentDirectoryErrorCode.CANNOT_PROCESS, e.toString()); // NOSONAR
 		}
 	}
+
+	private static final Pattern CONTAINS = Pattern.compile("(.+)\\s+contains\\s+\"(.+)\"");
 
 	@Override
 	public BrowseResult search (final String containerId, final String searchCriteria,
 			final String filter, final long firstResult, final long maxResults,
 			final SortCriterion[] orderBy) throws ContentDirectoryException {
-		// You can override this method to implement searching!
-		return super.search(containerId, searchCriteria, filter, firstResult, maxResults, orderBy);
+		LOG.info("search: {}, {}, {} ({}, {}, {})", containerId, searchCriteria, filter, firstResult, maxResults, Arrays.toString(orderBy));
+		try {
+			final ContentNode contentNode = this.contentTree.getNode(containerId);
+			if (contentNode == null) return new BrowseResult("", 0, 0);
+			if (contentNode.isItem()) throw new ContentDirectoryException(UNSUPPORTED_SEARCH_CONTAINER, "Can not seach inside in an item.");
+
+			// FIXME Force parse a title query ignoring everything else.
+			String term = null;
+			for (final String part : searchCriteria.replaceAll("[\\(\\)]", "").split("(?i)\\band\\b|\\bor\\b")) {
+				final Matcher m = CONTAINS.matcher(part.trim());
+				if (m.matches() && "dc:title".equalsIgnoreCase(m.group(1))) {
+					term = m.group(2);
+					break;
+				}
+			}
+			if (term == null || term.length() < 1) throw new ContentDirectoryException(UNSUPPORTED_SEARCH_CRITERIA, "Do not know how to parse: " + searchCriteria);
+			final List<Item> results = filterByTitleSubstring(contentNode.getContainer(), term.toLowerCase());
+
+			return toRangedResult(Collections.<Container> emptyList(), results, firstResult, maxResults);
+		}
+		catch (final ContentDirectoryException e) {
+			LOG.warn(String.format("Failed to parse search request" +
+					" (containerId=%s, searchCriteria=%s, filter=%s, firstResult=%s, maxResults=%s, orderby=%s).",
+					containerId, searchCriteria, filter, firstResult, maxResults, Arrays.toString(orderBy)));
+			throw e;
+		}
+		catch (final Exception e) {
+			LOG.warn(String.format("Failed to generate search results" +
+					" (containerId=%s, searchCriteria=%s, filter=%s, firstResult=%s, maxResults=%s, orderby=%s).",
+					containerId, searchCriteria, filter, firstResult, maxResults, Arrays.toString(orderBy)), e);
+			throw new ContentDirectoryException(ErrorCode.ACTION_FAILED, e.toString());
+		}
 	}
+
+	private static BrowseResult toRangedResult (final List<Container> containers, final List<Item> items, final long firstResult, final long maxResults) throws Exception {
+		final DIDLContent didl = new DIDLContent();
+		if (containers.size() > firstResult) {
+			final int from = (int) firstResult;
+			final int to = Math.min((int) (firstResult + maxResults), containers.size());
+			didl.setContainers(containers.subList(from, to));
+		}
+		if (didl.getContainers().size() < maxResults) {
+			final int from = (int) Math.max(firstResult - containers.size(), 0);
+			final int to = Math.min(items.size(), from + (int) (maxResults - didl.getContainers().size()));
+			didl.setItems(items.subList(from, to));
+		}
+		return new BrowseResult(new DIDLParser().generate(didl),
+				didl.getContainers().size() + didl.getItems().size(),
+				containers.size() + items.size());
+	}
+
+	/**
+	 * Lazy recursive impl.
+	 */
+	private static List<Item> filterByTitleSubstring (final Container container, final String lcaseTerm) {
+		final List<Item> results = new ArrayList<>();
+		for (final Item ci : container.getItems()) {
+			if (ci.getTitle().toLowerCase(Locale.ENGLISH).contains(lcaseTerm)) results.add(ci);
+		}
+		if (container.getContainers() != null) {
+			for (final Container childContainer : container.getContainers()) {
+				results.addAll(filterByTitleSubstring(childContainer, lcaseTerm));
+			}
+		}
+		return results;
+	}
+
 }
--- src/test/java/com/vaguehope/dlnatoad/dlnaserver/ContentDirectoryServiceTest.java
+++ src/test/java/com/vaguehope/dlnatoad/dlnaserver/ContentDirectoryServiceTest.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -43,11 +44,11 @@ public class ContentDirectoryServiceTest {
 
 	@Test
 	public void itReturnsAllItemsWhenTheyAreInsideTheRequetsRange () throws Exception {
-		List<ContentNode> dirs = givenMockDirs(3);
-		List<ContentNode> items = givenMockItems(3);
+		final List<ContentNode> dirs = givenMockDirs(3);
+		final List<ContentNode> items = givenMockItems(3);
 		addMockItem("item other", dirs.get(1));
 
-		BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 10, null);
+		final BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 10, null);
 
 		assertCorrectResult(ret, 6, 6);
 		assertParserMarshaled(dirs, items);
@@ -55,11 +56,11 @@ public class ContentDirectoryServiceTest {
 
 	@Test
 	public void itDoesNotMindIfMaxIsMoreThanCount () throws Exception {
-		List<ContentNode> dirs = givenMockDirs(3);
-		List<ContentNode> items = givenMockItems(3);
+		final List<ContentNode> dirs = givenMockDirs(3);
+		final List<ContentNode> items = givenMockItems(3);
 		addMockItem("item other", dirs.get(1));
 
-		BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 20, null);
+		final BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 20, null);
 
 		assertCorrectResult(ret, 6, 6);
 		assertParserMarshaled(dirs, items);
@@ -67,9 +68,9 @@ public class ContentDirectoryServiceTest {
 
 	@Test
 	public void itReturnsFirstFewDirsWhenOnlyDirsInsideRange () throws Exception {
-		List<ContentNode> dirs = givenMockDirs(5);
+		final List<ContentNode> dirs = givenMockDirs(5);
 
-		BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 3, null);
+		final BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 3, null);
 
 		assertCorrectResult(ret, 3, 5);
 		assertParserMarshaled(dirs.subList(0, 3), null);
@@ -77,9 +78,9 @@ public class ContentDirectoryServiceTest {
 
 	@Test
 	public void itReturnsFirstItemsWhenOnlyItemsInsideRange () throws Exception {
-		List<ContentNode> items = givenMockItems(10);
+		final List<ContentNode> items = givenMockItems(10);
 
-		BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 3, null);
+		final BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 3, null);
 
 		assertCorrectResult(ret, 3, 10);
 		assertParserMarshaled(null, items.subList(0, 3));
@@ -87,10 +88,10 @@ public class ContentDirectoryServiceTest {
 
 	@Test
 	public void itReturnsDirsThenSomeItemsWhenNotAllItemsFitInsideRange () throws Exception {
-		List<ContentNode> dirs = givenMockDirs(5);
-		List<ContentNode> items = givenMockItems(10);
+		final List<ContentNode> dirs = givenMockDirs(5);
+		final List<ContentNode> items = givenMockItems(10);
 
-		BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 10, null);
+		final BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 10, null);
 
 		assertCorrectResult(ret, 10, 15);
 		assertParserMarshaled(dirs, items.subList(0, 5));
@@ -98,10 +99,10 @@ public class ContentDirectoryServiceTest {
 
 	@Test
 	public void itReturnsMidRangeDirsWhenOnlyTheyFallInsideRange () throws Exception {
-		List<ContentNode> dirs = givenMockDirs(10);
+		final List<ContentNode> dirs = givenMockDirs(10);
 		givenMockItems(10);
 
-		BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 3, 3, null);
+		final BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 3, 3, null);
 
 		assertCorrectResult(ret, 3, 20);
 		assertParserMarshaled(dirs.subList(3, 6), null);
@@ -109,10 +110,10 @@ public class ContentDirectoryServiceTest {
 
 	@Test
 	public void itReturnsLastFewDirsThenSomeItems () throws Exception {
-		List<ContentNode> dirs = givenMockDirs(10);
-		List<ContentNode> items = givenMockItems(10);
+		final List<ContentNode> dirs = givenMockDirs(10);
+		final List<ContentNode> items = givenMockItems(10);
 
-		BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 5, 10, null);
+		final BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 5, 10, null);
 
 		assertCorrectResult(ret, 10, 20);
 		assertParserMarshaled(dirs.subList(5, 10), items.subList(0, 5));
@@ -121,9 +122,9 @@ public class ContentDirectoryServiceTest {
 	@Test
 	public void itReturnsMidRangeItems () throws Exception {
 		givenMockDirs(10);
-		List<ContentNode> items = givenMockItems(10);
+		final List<ContentNode> items = givenMockItems(10);
 
-		BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 14, 2, null);
+		final BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 14, 2, null);
 
 		assertCorrectResult(ret, 2, 20);
 		assertParserMarshaled(null, items.subList(4, 6));
@@ -132,9 +133,9 @@ public class ContentDirectoryServiceTest {
 	@Test
 	public void itReturnsLastFewItems () throws Exception {
 		givenMockDirs(10);
-		List<ContentNode> items = givenMockItems(10);
+		final List<ContentNode> items = givenMockItems(10);
 
-		BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 15, 5, null);
+		final BrowseResult ret = this.undertest.browse(this.contentTree.getRootNode().getId(), BrowseFlag.DIRECT_CHILDREN, null, 15, 5, null);
 
 		assertCorrectResult(ret, 5, 20);
 		assertParserMarshaled(null, items.subList(5, 10));
@@ -142,57 +143,89 @@ public class ContentDirectoryServiceTest {
 
 	@Test
 	public void itReturnsItemsFromSubDir () throws Exception {
-		List<ContentNode> rootDirs = givenMockDirs(5);
-		List<ContentNode> items = givenMockItems(15, rootDirs.get(0));
+		final List<ContentNode> rootDirs = givenMockDirs(5);
+		final List<ContentNode> items = givenMockItems(15, rootDirs.get(0));
 
-		BrowseResult ret = this.undertest.browse(rootDirs.get(0).getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 10, null);
+		final BrowseResult ret = this.undertest.browse(rootDirs.get(0).getId(), BrowseFlag.DIRECT_CHILDREN, null, 0, 10, null);
 
 		assertCorrectResult(ret, 10, 15);
 		assertParserMarshaled(null, items.subList(0, 10));
 	}
 
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Search.
 
-	private List<ContentNode> givenMockDirs (int n) {
-		List<ContentNode> ret = new ArrayList<ContentNode>();
+	/*
+	 * (upnp:class derivedfrom "object.item.videoItem" and dc:title contains "daa")
+	 * (upnp:class derivedfrom "object.item.audioItem" and dc:title contains "daa")
+	 * (upnp:class derivedfrom "object.item.audioItem" and (dc:creator contains "daa" or upnp:artist contains "daa"))
+	 * (upnp:class = "object.container.album.musicAlbum" and dc:title contains "daa")
+	 * (upnp:class = "object.container.person.musicArtist" and dc:title contains "daa")
+	 */
+
+	@Test
+	public void itSearchesByTitle () throws Exception {
+		final List<ContentNode> items = givenMockItems(10);
+
+		when(items.get(3).getItem().getTitle()).thenReturn("some file fooBar song.mp4");
+
+		final BrowseResult ret = this.undertest.search(this.contentTree.getRootNode().getId(),
+				"(upnp:class derivedfrom \"object.item.videoItem\" and dc:title contains \"foobar\")",
+				"*", 0, 3, null);
+
+		assertCorrectResult(ret, 1, 1);
+		assertParserMarshaled(null, items.subList(3, 4));
+	}
+
+	@Ignore("not implemented")
+	@Test
+	public void itSearchesByClass () throws Exception {
+		// TODO
+	}
+
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	private List<ContentNode> givenMockDirs (final int n) {
+		final List<ContentNode> ret = new ArrayList<ContentNode>();
 		for (int i = 0; i < n; i++) {
 			ret.add(addMockDir("dir " + i));
 		}
 		return ret;
 	}
 
-	private List<ContentNode> givenMockItems (int n) {
+	private List<ContentNode> givenMockItems (final int n) {
 		return givenMockItems(n, this.contentTree.getRootNode());
 	}
 
-	private List<ContentNode> givenMockItems (int n, ContentNode parent) {
-		List<ContentNode> ret = new ArrayList<ContentNode>();
+	private List<ContentNode> givenMockItems (final int n, final ContentNode parent) {
+		final List<ContentNode> ret = new ArrayList<ContentNode>();
 		for (int i = 0; i < n; i++) {
 			ret.add(addMockItem("item " + i, parent));
 		}
 		return ret;
 	}
 
-	private ContentNode addMockDir (String id) {
+	private ContentNode addMockDir (final String id) {
 		return addMockDir(id, this.contentTree.getRootNode());
 	}
 
-	private ContentNode addMockDir (String id, ContentNode parent) {
-		Container container = new Container();
+	private ContentNode addMockDir (final String id, final ContentNode parent) {
+		final Container container = new Container();
 		container.setId(id);
 		container.setChildCount(Integer.valueOf(0));
 
-		ContentNode node = new ContentNode(id, container);
+		final ContentNode node = new ContentNode(id, container);
 		this.contentTree.addNode(node);
 		parent.getContainer().addContainer(node.getContainer());
 		parent.getContainer().setChildCount(parent.getContainer().getChildCount() + 1);
 		return node;
 	}
 
-	private ContentNode addMockItem (String id, ContentNode parent) {
-		Item item = mock(Item.class);
+	private ContentNode addMockItem (final String id, final ContentNode parent) {
+		final Item item = mock(Item.class);
+		when(item.getTitle()).thenReturn("item " + id);
 		when(item.toString()).thenReturn("item " + id);
-		ContentNode node = new ContentNode(id, item, mock(File.class));
+		final ContentNode node = new ContentNode(id, item, mock(File.class));
 		this.contentTree.addNode(node);
 		parent.getContainer().addItem(node.getItem());
 		parent.getContainer().setChildCount(parent.getContainer().getChildCount() + 1);
@@ -200,39 +233,39 @@ public class ContentDirectoryServiceTest {
 	}
 
 	private static DIDLParser mockDidlParser () throws Exception {
-		DIDLParser didlParser = mock(DIDLParser.class);
+		final DIDLParser didlParser = mock(DIDLParser.class);
 		PowerMockito.whenNew(DIDLParser.class).withNoArguments().thenReturn(didlParser);
 		when(didlParser.generate(isA(DIDLContent.class))).thenReturn(DIDL_XML);
 		return didlParser;
 	}
 
-	private static void assertCorrectResult (BrowseResult ret, long count, long totalMatches) {
+	private static void assertCorrectResult (final BrowseResult ret, final long count, final long totalMatches) {
 		assertEquals(count, ret.getCountLong());
 		assertEquals(totalMatches, ret.getTotalMatchesLong());
 		assertEquals(DIDL_XML, ret.getResult());
 	}
 
-	private void assertParserMarshaled (List<ContentNode> dirs, List<ContentNode> items) throws Exception {
-		ArgumentCaptor<DIDLContent> cap = ArgumentCaptor.forClass(DIDLContent.class);
+	private void assertParserMarshaled (final List<ContentNode> dirs, final List<ContentNode> items) throws Exception {
+		final ArgumentCaptor<DIDLContent> cap = ArgumentCaptor.forClass(DIDLContent.class);
 		verify(this.didlParser).generate(cap.capture());
 		assertEquals(listOfContainers(dirs), cap.getValue().getContainers());
 		assertEquals(listOfItems(items), cap.getValue().getItems());
 	}
 
-	private static List<Container> listOfContainers (List<ContentNode> nodes) {
-		List<Container> l = new ArrayList<Container>();
+	private static List<Container> listOfContainers (final List<ContentNode> nodes) {
+		final List<Container> l = new ArrayList<Container>();
 		if (nodes != null) {
-			for (ContentNode cn : nodes) {
+			for (final ContentNode cn : nodes) {
 				l.add(cn.getContainer());
 			}
 		}
 		return l;
 	}
 
-	private static List<Item> listOfItems (List<ContentNode> nodes) {
-		List<Item> l = new ArrayList<Item>();
+	private static List<Item> listOfItems (final List<ContentNode> nodes) {
+		final List<Item> l = new ArrayList<Item>();
 		if (nodes != null) {
-			for (ContentNode cn : nodes) {
+			for (final ContentNode cn : nodes) {
 				l.add(cn.getItem());
 			}
 		}
