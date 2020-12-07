9b12ba116cc1da61c8bfdabb80b4a9ec0fd6775f
haku <git@haku.me>
2020-12-07 21:28:42 +0000

    Fix binding to docker network.

--- src/main/java/com/vaguehope/dlnatoad/util/NetHelper.java
+++ src/main/java/com/vaguehope/dlnatoad/util/NetHelper.java
@@ -8,6 +8,7 @@ import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Locale;
 
 public final class NetHelper {
 
@@ -19,7 +20,7 @@ public final class NetHelper {
 		final List<InetAddress> addresses = new ArrayList<InetAddress>();
 		for (final Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces(); interfaces.hasMoreElements();) {
 			final NetworkInterface iface = interfaces.nextElement();
-			if (iface.isLoopback()) continue;
+			if (!isUseable(iface)) continue;
 			for (final InterfaceAddress ifaceAddr : iface.getInterfaceAddresses()) {
 				final InetAddress inetAddr = ifaceAddr.getAddress();
 				if (!(inetAddr instanceof Inet4Address)) continue;
@@ -27,6 +28,16 @@ public final class NetHelper {
 			}
 		}
 		return addresses;
+	}
+
+	private static boolean isUseable (final NetworkInterface iface) throws SocketException {
+		if (iface.isLoopback()) return false;
+
+		final String name = iface.getName().toLowerCase(Locale.ROOT);
+		if (name == null) return false;
+		if (name.startsWith("docker")) return false;
+
+		return true;
 	}
 
 }
