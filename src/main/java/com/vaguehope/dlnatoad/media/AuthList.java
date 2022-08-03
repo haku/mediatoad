b433bb134c6fa59d2783f2637307ae58fa880d3a
haku <git@haku.me>
2022-08-03 15:01:06 +0100

    Minimal caching of AUTH files.

--- src/main/java/com/vaguehope/dlnatoad/media/AuthList.java
+++ src/main/java/com/vaguehope/dlnatoad/media/AuthList.java
@@ -6,12 +6,17 @@ import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.vaguehope.dlnatoad.C;
 
 public class AuthList {
@@ -26,20 +31,32 @@ public class AuthList {
 	 */
 	public static AuthList forDir(final File dir) throws IOException {
 		if (dir == null) throw new IllegalArgumentException("Can not load auth list for null dir.");
+		if (!dir.exists() || !dir.isDirectory()) {
+			throw new IOException("Can not load AUTH file for non existing directory: " + dir.getAbsolutePath());
+		}
 
-		final Set<String> names = readNamesForDir(dir);
+		final Set<String> names = getNamesForDir(dir);
 		if (names == null) return null;
 		if (names.isEmpty()) return EMPTY_AUTH_LIST;
 		return new AuthList(names);
 	}
 
-	private static Set<String> readNamesForDir(final File dir) throws IOException {
-		if (!dir.exists() || !dir.isDirectory()) {
-			throw new IOException("Can not load AUTH file for non existing directory: " + dir.getAbsolutePath());
-		}
+	private static final Cache<String, Optional<Set<String>>> AUTH_FILE_CACHE = CacheBuilder.newBuilder()
+			.expireAfterWrite(5, TimeUnit.SECONDS)
+			.build();
 
+	private static Set<String> getNamesForDir(final File dir) throws IOException {
+		try {
+			return AUTH_FILE_CACHE.get(dir.getAbsolutePath(), () -> Optional.ofNullable(readNamesForDir(dir))).orElse(null);
+		}
+		catch (final ExecutionException e) {
+			throw new IOException(e);
+		}
+	}
+
+	private static Set<String> readNamesForDir(final File dir) throws IOException {
 		final File parentDir = dir.getAbsoluteFile().getParentFile();
-		final Set<String> parentNames = parentDir != null ? readNamesForDir(parentDir) : null;
+		final Set<String> parentNames = parentDir != null ? getNamesForDir(parentDir) : null;
 
 		final File authFile = new File(dir, AUTH_FILE_NAME);
 		if (!authFile.exists()) return parentNames;
