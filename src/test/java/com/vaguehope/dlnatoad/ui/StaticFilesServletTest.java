143ca4953f61cc2e57f52a0693736a18bcc4b6a9
haku <git@haku.me>
2024-02-16 19:24:12 +0000

    Extra test for static file serving.

--- src/test/java/com/vaguehope/dlnatoad/ui/StaticFilesServletTest.java
+++ src/test/java/com/vaguehope/dlnatoad/ui/StaticFilesServletTest.java
@@ -4,24 +4,37 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.vaguehope.dlnatoad.util.RequestLoggingFilter;
 
 public class StaticFilesServletTest {
+
+	@Rule public TemporaryFolder tmp = new TemporaryFolder();
 
 	private StaticFilesServlet undertest;
 	private Server server;
@@ -49,6 +62,36 @@ public class StaticFilesServletTest {
 		assertEquals("text/plain", actualType.get(0));
 	}
 
+	@Test
+	public void itHandlesModifiedFile() throws Exception {
+		final File root = this.tmp.newFolder("webroot");
+		final String filename = "modified.txt";
+		final File file = new File(root, filename);
+		final CharSequence text = String.valueOf(System.currentTimeMillis());
+		FileUtils.write(file, text, StandardCharsets.UTF_8);
+
+		final long oldTime = System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(1);
+		assertTrue(file.setLastModified(oldTime));
+
+		this.undertest = new StaticFilesServlet(root);
+		startServer();
+
+		assertRequestResponseCode(filename, 0, 200);
+		assertRequestResponseCode(filename, oldTime, 304);
+
+		assertTrue(file.setLastModified(System.currentTimeMillis()));
+		assertRequestResponseCode(filename, oldTime, 200);
+	}
+
+	private void assertRequestResponseCode(final String filename, final long ifModifiedSince, final int code) throws MalformedURLException, IOException {
+		final URL url = new URL("http://" + this.hostAddress + ":"
+				+ ((ServerConnector) this.server.getConnectors()[0]).getLocalPort()
+				+ "/w/" + filename);
+		final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		if (ifModifiedSince > 0) conn.setIfModifiedSince(ifModifiedSince);
+		assertEquals(code, conn.getResponseCode());
+	}
+
 	@SuppressWarnings("resource")
 	private void startServer() throws Exception {
 		this.server = new Server();
@@ -61,6 +104,7 @@ public class StaticFilesServletTest {
 
 		final ServletContextHandler contextHandler = new ServletContextHandler();
 		contextHandler.setContextPath("/");
+		contextHandler.addFilter(new FilterHolder(new RequestLoggingFilter()), "/*", null);
 		contextHandler.addServlet(new ServletHolder(this.undertest), "/w/*");
 		this.server.setHandler(contextHandler);
 		this.server.start();
