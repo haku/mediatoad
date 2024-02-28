77ea36bfc7914373b1886d2b3caa173e821117ae
haku <git@haku.me>
2024-02-28 13:17:00 +0000

    Microbenchmark different ways of using mustache templates.

--- src/test/java/com/vaguehope/dlnatoad/ui/DirServletTest.java
+++ src/test/java/com/vaguehope/dlnatoad/ui/DirServletTest.java
@@ -5,19 +5,29 @@ import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayInputStream;
+import java.io.CharArrayWriter;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.teleal.common.mock.http.MockHttpServletRequest;
 import org.teleal.common.mock.http.MockHttpServletResponse;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.reflect.ReflectionObjectHandler;
+import com.google.common.collect.ImmutableMap;
 import com.vaguehope.dlnatoad.media.ContentItem;
 import com.vaguehope.dlnatoad.media.ContentNode;
 import com.vaguehope.dlnatoad.media.ContentServingHistory;
@@ -91,6 +101,81 @@ public class DirServletTest {
 		}
 
 		assertEquals(expectedNames, actualNames);
+	}
+
+	/**
+	 * mapScope:
+	 * ReflectionObjectHandler: 10000 in 1800ms = 0.18ms per template
+	 * SimpleObjectHandler:     10000 in 1400ms = 0.14ms per template
+	 *
+	 * classScope:
+	 * ReflectionObjectHandler: 10000 in 1250ms = 0.13ms per template
+	 * SimpleObjectHandler:     10000 in 1900ms = 0.19ms per template
+	 * 
+	 * also classScope is 10x faster to build than mapScope.
+	 */
+	@Ignore
+	@SuppressWarnings("resource")
+	@Test
+	public void itDoesAMicroBenckmark() throws Exception {
+		final String template = "<ul>{{#list_items}}\n"
+				+ "<li><a href=\"{{path_prefix}}{{path}}\">{{title}}</a></li>\n"
+				+ "{{/list_items}}</ul>\n";
+
+		final DefaultMustacheFactory mf = new DefaultMustacheFactory();
+
+		// Choose 1 of these:
+		mf.setObjectHandler(new ReflectionObjectHandler());
+//		mf.setObjectHandler(new SimpleObjectHandler());
+
+		final long start1 = System.nanoTime();
+		final Map<String, Object> mapScope = new HashMap<>();
+		final List<Map<String, String>> listItems = new ArrayList<>();
+		mapScope.put("list_items", listItems);
+		for (int i = 0; i < 500; i++) {
+			listItems.add(ImmutableMap.of("path", "id" + i, "title", "title" + i));
+		}
+		final long duration1 = System.nanoTime() - start1;
+		System.out.println("duration1: " + TimeUnit.NANOSECONDS.toNanos(duration1));
+
+		final long start2 = System.nanoTime();
+		final ViewClass classScope = new ViewClass();
+		for (int i = 0; i < 500; i++) {
+			final RowClass row = new RowClass("id" + i, "title" + i);
+			classScope.list_items.add(row);
+		}
+		final long duration2 = System.nanoTime() - start2;
+		System.out.println("duration2: " + TimeUnit.NANOSECONDS.toNanos(duration2));
+
+		// Chose 1 of these:
+//		final Object scope = mapScope;
+		final Object scope = classScope;
+
+		final Mustache mTemplate = mf.compile(new StringReader(template), "template");
+		final CharArrayWriter output = new CharArrayWriter();
+		mTemplate.execute(output, scope).flush();
+
+		final long start3 = System.nanoTime();
+		for (int i = 0; i < 10000; i++) {
+			output.reset();
+			mTemplate.execute(output, scope).flush();
+		}
+		final long duration3 = System.nanoTime() - start3;
+		System.out.println("duration3: " + TimeUnit.NANOSECONDS.toMillis(duration3));
+	}
+
+	public static class ViewClass {
+		public final List<RowClass> list_items = new ArrayList<>();
+	}
+
+	public static class RowClass {
+		public final String path;
+		public final String title;
+		public RowClass(final String path, final String title) {
+			this.path = path;
+			this.title = title;
+		}
+
 	}
 
 }
