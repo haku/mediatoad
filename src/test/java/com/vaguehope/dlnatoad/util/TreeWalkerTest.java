ed8ee8caee350910f78543c51b9fae4a5cb885fd
haku <git@haku.me>
2024-03-04 21:02:10 +0000

    Switch from Java 1.8 to 11.

--- src/test/java/com/vaguehope/dlnatoad/util/TreeWalkerTest.java
+++ src/test/java/com/vaguehope/dlnatoad/util/TreeWalkerTest.java
@@ -1,6 +1,6 @@
 package com.vaguehope.dlnatoad.util;
 
-import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.isA;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
