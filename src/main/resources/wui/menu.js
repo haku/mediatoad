e7572f54ab46a077c62bced5fe4857e14a50c913
haku <git@haku.me>
2025-07-16 16:04:32 +0100

    put multiple login buttons in a dropdown thingy.

--- src/main/resources/wui/menu.js
+++ src/main/resources/wui/menu.js
@@ -2,6 +2,7 @@ MenuHelper = {};
 (function() {
   MenuHelper.setup = (holder_id) => {
     const btn = document.querySelector('#' + holder_id + '>button');
+    if (!btn) return;
     const drop = document.querySelector('#' + holder_id + '>div');
     btn.addEventListener('click', (event) => {
       event.preventDefault();
@@ -18,5 +19,6 @@ MenuHelper = {};
 })();
 
 (function() {
+  MenuHelper.setup('login_menu');
   MenuHelper.setup('search_menu');
 })();
