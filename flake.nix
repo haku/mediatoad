5ea7cbbea178b6216bb70fe741ea6d2e8e136dc2
fae <git@haku.me>
2026-07-07 07:09:46 +0000

    nix flake update to 26.05.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-compat.url = "github:NixOS/flake-compat";
     flake-parts.url = "github:hercules-ci/flake-parts";
     make-shell = {
